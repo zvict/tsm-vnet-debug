8889dd7c5f4ecba16c0277198b7835fe56648fc0
Yanshu Zhang <ustczys@mail.ustc.edu.cn>
2020-10-30 15:36:29 +0800

    add vnet in ConsensusModule

--- ops/basic_ops.py
+++ ops/basic_ops.py
@@ -8,11 +8,12 @@ class Identity(torch.nn.Module):
 
 class SegmentConsensus(torch.nn.Module):
 
-    def __init__(self, consensus_type, dim=1):
+    def __init__(self, consensus_type, dim=1, vnet=None):
         super(SegmentConsensus, self).__init__()
         self.consensus_type = consensus_type
         self.dim = dim
         self.shape = None
+        self.vnet = vnet
 
     def forward(self, input_tensor):
         self.shape = input_tensor.size()
@@ -20,6 +21,8 @@ class SegmentConsensus(torch.nn.Module):
             output = input_tensor.mean(dim=self.dim, keepdim=True)
         elif self.consensus_type == 'identity':
             output = input_tensor
+        elif self.consensus_type == 'vnet':
+            output = vnet(input_tensor)
         else:
             output = None
 
@@ -28,10 +31,11 @@ class SegmentConsensus(torch.nn.Module):
 
 class ConsensusModule(torch.nn.Module):
 
-    def __init__(self, consensus_type, dim=1):
+    def __init__(self, consensus_type, dim=1, vnet=None):
         super(ConsensusModule, self).__init__()
         self.consensus_type = consensus_type if consensus_type != 'rnn' else 'identity'
         self.dim = dim
+        self.vnet = vnet
 
     def forward(self, input):
-        return SegmentConsensus(self.consensus_type, self.dim)(input)
+        return SegmentConsensus(self.consensus_type, self.dim, self.vnet)(input)
